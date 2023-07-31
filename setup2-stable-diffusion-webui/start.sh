13d5f7200a299c2509a8f346795e8db8d2edc3fd
guoyunpeng <guoyunpeng@thinkit.cn>
2023-07-31 15:20:48 +0800

    update

--- setup2-stable-diffusion-webui/start.sh
+++ setup2-stable-diffusion-webui/start.sh
@@ -4,9 +4,23 @@
 path=$(cd `dirname $0`; pwd)
 
 
+models="$path/models"
+outputs="$path/outputs"
+
+
+if [ ! -d "$models" ]; then
+    mkdir -p $models
+    chmod 777 $models
+fi
+if [ ! -d "$outputs" ]; then
+    mkdir -p $outputs
+    chmod 777 $outputs
+fi
+
+
 docker run -d --name webui --gpus all -p 7860:7860 -p 2222:22 \
-        -v $path/models:/home/webui/stable-diffusion-webui/models \
-        -v $path/outputs:/home/webui/stable-diffusion-webui/outputs \
+        -v $models:/home/webui/stable-diffusion-webui/models \
+        -v $outputs:/home/webui/stable-diffusion-webui/outputs \
 	webui sleep infinity
 
 
