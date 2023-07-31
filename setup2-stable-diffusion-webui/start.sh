83c7c4994ac694eab15056c7386743ee6b93e07a
cloudp1216 <cloudp1216@gmail.com>
2023-07-31 12:00:08 +0800

    update

--- setup2-stable-diffusion-webui/start.sh
+++ setup2-stable-diffusion-webui/start.sh
@@ -4,10 +4,10 @@
 path=$(cd `dirname $0`; pwd)
 
 
-docker run -d --name webui --gpus all \
+docker run -d --name webui --gpus all -p 7860:7860 -p 2222:22 \
         -v $path/models:/home/webui/stable-diffusion-webui/models \
         -v $path/outputs:/home/webui/stable-diffusion-webui/outputs \
-	sd sleep infinity
+	webui sleep infinity
 
 
 echo "Please enter the container to execute: ./webui.sh --xformers --listen"
