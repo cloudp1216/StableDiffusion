44653c9fb37622017688158e934340dba3040d41
guoyunpeng <guoyunpeng@thinkit.cn>
2023-12-18 17:33:44 +0800

    update

--- setup2-stable-diffusion-webui/start.sh
+++ setup2-stable-diffusion-webui/start.sh
@@ -22,16 +22,23 @@ do
     fi
 done
 
-docker run -d --restart=unless-stopped --name webui --gpus all -p 7860:7860 -p 2222:22 \
-    -v $models:/home/webui/stable-diffusion-webui/models \
-    -v $outputs:/home/webui/stable-diffusion-webui/outputs \
-    -v $embeddings:/home/webui/stable-diffusion-webui/embeddings \
-    -v $extensions:/home/webui/stable-diffusion-webui/extensions \
-    webui sleep infinity
+function run() {
+    docker run -d --restart=unless-stopped --name webui --gpus all -p 7860:7860 -p 2222:22 \
+        -v $models:/home/webui/stable-diffusion-webui/models \
+        -v $outputs:/home/webui/stable-diffusion-webui/outputs \
+        -v $embeddings:/home/webui/stable-diffusion-webui/embeddings \
+        -v $extensions:/home/webui/stable-diffusion-webui/extensions \
+        webui sleep infinity
+}
 
-echo "Please execute:"
-echo "    1. docker exec -it webui bash"
-echo "    2. cd stable-diffusion-webui"
-echo "    3. ./webui.sh --xformers --listen --enable-insecure-extension-access --theme=dark"
+if [ `docker ps -a|grep webui -c 2>/dev/null` == "0" ]; then
+    run
+    echo "Please execute:"
+    echo "    1. docker exec -it webui bash"
+    echo "    2. cd stable-diffusion-webui"
+    echo "    3. ./webui.sh --xformers --listen --enable-insecure-extension-access --theme=dark"
+else
+    echo "Containers webui is existed."
+fi
 
 
